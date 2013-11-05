2e5510a563b1862072b909092dddc714aa8e2e09
feng.zhang <cst.feng@gmail.com>
2013-11-05 21:38:24 +0900

    Initial Commit

---
+++ ObjectiveC-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectiveC-1
+//
+//  Created by 張 峰 on 2013/11/05.
+//  Copyright (c) 2013年 張 峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
